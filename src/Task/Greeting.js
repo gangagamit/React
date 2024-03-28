2472a0b06ed23f3879e23b1dd35c7c2b607435c7
gangagamit <gangagamit350@gmail.com>
2024-03-28 09:38:35 +0530

    small change

--- src/Task/Greeting.js
+++ src/Task/Greeting.js
@@ -15,7 +15,7 @@ function Name() {
   return ( 
     <> 
     <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fphotos-images%2Fsurat-city.html&psig=AOvVaw2rrgnwVH5XSl3KnKnqX_Fn&ust=1711681785862000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCt3Nr9lYUDFQAAAAAdAAAAABAE" alt="Image"></img>
-      <h1 className="text-4xl text-center absolute top-[180px] left-[350px]">Hello :{timeOfDay}!</h1> 
+      <h1  className="text-4xl text-center absolute top-[180px] left-[350px]">Hello :{timeOfDay}!</h1> 
     </> 
   ) 
 }; 
