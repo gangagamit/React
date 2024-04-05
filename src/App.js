fb8a71d524c28b3e06162f5b77852f9c585cb32c
gangagamit <gangagamit350@gmail.com>
2024-04-05 12:17:11 +0530

    5-4-2024

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import './App.css';
-import Heading from  './component/Heading';
-import Pera from './component/Pera';
-import Img from './component/Image';
-import List from './component/List';
-import Pera1 from './component/Pera1';
-import CompA from './component/CompA';
+// import Heading from  './component/Heading';
+// import Pera from './component/Pera';
+// import Img from './component/Image';
+// import List from './component/List';
+// import Pera1 from './component/Pera1';
+// import CompA from './component/CompA';
 // import Name from './Task/Greeting';
 // import CardProps from './Task/CardProps';
-import Props from './component/Props';
+// import Props from './component/Props';
 // import Img1 from './Image/Img-1.jpg';
 // import Img2 from './Image/Img-2.jpg';
 // import Img3 from './Image/Img-3.jpg';
 // import Data from './Task/Data';
 // import Title from './Task/Title';
 // import Footer from './Task/Footer';
-import One from './component/One';
-import Two from './component/Two';
-import Onclick from './component/Onclick';
-import React,{ useState } from 'react';
+// import One from './component/One';
+// import Two from './component/Two';
+// import Onclick from './component/Onclick';
+import ClassConponent from './component/ClassConponent';
+// import React,{ useState } from 'react';
 function App() {
-  const [count,setCount] = useState(0)
-  const myCount= ()=>{
-    console.log("click")
-    setCount(count + 1)
-  }
+  // const [count,setCount] = useState(0)
+  // const myCount= ()=>{
+  //   console.log("click")
+  //   setCount(count + 1)
+  // }
   return (
     <>
       
       {/* <Title/> */}
     <div className="App">
-      <h3>{count}</h3>
-      <button onClick={myCount}>Click Me!!</button>
+      <ClassConponent/>
+      {/* <h3>{count}</h3>
+      <button onClick={myCount}>Click Me!!</button> */}
       {/* <Heading/>
       <Pera/>
       <Img/>
@@ -40,7 +42,7 @@ function App() {
        {/* <Name/> */}
        {/* {((false)?<One/>:<Two/>)}
       <Props Name="Ganga" age="24"/> */}
-      <Onclick/>
+      {/* <Onclick/> */}
       {/* {Data.map((cvalue,index,array)=>{
         console.log(cvalue,"cvalue")
         return <CardProps key={index} Name={cvalue.Name} descri={cvalue.descri} btn={cvalue.btn} img={cvalue.img}/>
